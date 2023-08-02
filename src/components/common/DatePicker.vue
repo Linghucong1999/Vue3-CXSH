b8667f07c4dd9f4dc84b8b745f0f963cbc8bcfa9
mika <2865911620@qq.com>
2023-08-02 11:06:15 +0800

    日期选择器

---
+++ src/components/common/DatePicker.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="demo-date-picker">
+    <div class="block">
+      <el-date-picker
+        v-model="modelValue"
+        :type="typeValue"
+        placeholder="选择日期"
+        range-separator="至"
+        start-placeholder="Start date"
+        end-placeholder="End date"
+        unlink-panels
+        :disabled-date="disabledDate"
+        :shortcuts="shortcuts"
+      ></el-date-picker>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    setType: String,
+    setSize: String,
+    setDateValue: Array,
+  },
+  data() {
+    return {
+      modelValue: this.setDateValue,
+      typeValue: this.setType,
+      sizeValue: this.setSize,
+      disabledDate(time) {
+        return time.getTime() > Date.now();
+      },
+      shortcuts: [
+        {
+          text: "上一周",
+          value: () => {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+            return [start, end];
+          },
+        },
+        {
+          text: "上一个月",
+          value: () => {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+            return [start, end];
+          },
+        },
+        {
+          text: "前三个月",
+          value: () => {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+            return [start, end];
+          },
+        },
+      ],
+    };
+  },
+  watch: {
+    setDateValue() {
+      this.modelValue = this.setDateValue;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>
