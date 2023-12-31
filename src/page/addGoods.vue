40ffc4b4e926a7ef633492c5c2f4521364ee72a7
mika <2865911620@qq.com>
2023-12-31 19:12:52 +0800

    style:弹窗样式的修改

--- src/page/addGoods.vue
+++ src/page/addGoods.vue
@@ -3,71 +3,42 @@
     <el-row>
       <el-col :span="16" :offset="4">
         <header class="form_header">选择食品种类</header>
-        <el-form
-          :model="categoryForm"
-          ref="categoryForm"
-          label-width="110px"
-          class="form"
-        >
+        <el-form :model="categoryForm" ref="categoryForm" label-width="110px" class="form">
           <el-row class="category_select">
             <el-form-item label="食品种类">
-              <el-select
-                v-model="categoryForm.categorySelect"
-                :placeholder="selectValue.label"
-                style="width: 100%"
-              >
-                <el-option
-                  v-for="item in categoryForm.categoryList"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
-                ></el-option>
+              <el-select v-model="categoryForm.categorySelect" :placeholder="selectValue.label" style="width: 100%">
+                <el-option v-for="item in categoryForm.categoryList" :key="item.value" :label="item.label"
+                  :value="item.value"></el-option>
               </el-select>
             </el-form-item>
           </el-row>
-          <el-row
-            class="add_category_row"
-            :class="showAddCategory ? 'showEdit' : ''"
-          >
+          <el-row class="add_category_row" :class="showAddCategory ? 'showEdit' : ''">
             <div class="add_category">
               <el-form-item label="食品种类" prop="name">
-                <el-input
-                  v-model="categoryForm.name"
-                  placeholder="食品种类名称"
-                ></el-input>
+                <el-input v-model="categoryForm.name" placeholder="食品种类名称"></el-input>
               </el-form-item>
               <el-form-item label="种类描述">
                 <el-input v-model="categoryForm.description"></el-input>
               </el-form-item>
               <el-form-item>
-                <el-button
-                  type="primary"
-                  @click="submitcategoryForm('categoryForm')"
-                  >提交</el-button
-                >
+                <el-button type="primary" @click="submitcategoryForm('categoryForm')">提交</el-button>
               </el-form-item>
             </div>
           </el-row>
           <div class="add_category_button" @click="addCategoryFun">
-            <el-icon class="edit_icon" v-if="showAddCategory"
-              ><CaretTop></CaretTop
-            ></el-icon>
-            <el-icon class="edit_icon" v-else default="icon"
-              ><CaretBottom></CaretBottom
-            ></el-icon>
+            <el-icon class="edit_icon" v-if="showAddCategory">
+              <CaretTop></CaretTop>
+            </el-icon>
+            <el-icon class="edit_icon" v-else default="icon">
+              <CaretBottom></CaretBottom>
+            </el-icon>
             <span>添加食品种类</span>
           </div>
         </el-form>
 
         <!-- 添加食品 -->
         <header class="form_header">添加食品</header>
-        <el-form
-          :model="foodForm"
-          ref="foodForm"
-          :rules="foodrules"
-          label-width="110px"
-          class="form food_form"
-        >
+        <el-form :model="foodForm" ref="foodForm" :rules="foodrules" label-width="110px" class="form food_form">
           <el-form-item label="食品名称" prop="name">
             <el-input v-model="foodForm.name"></el-input>
           </el-form-item>
@@ -79,19 +50,12 @@
           </el-form-item>
 
           <el-form-item label="上传商品图片">
-            <el-upload
-              :action="baseUrl + '/v1/addimg/food'"
-              class="avatar-uploader"
-              :show-file-list="true"
-              :on-success="handleShopAvatarScucess"
-              :before-upload="beforeAvatarUpload"
-              :limit="1"
-              :on-exceed="handleExceed"
-              :on-remove="handleRemove"
-              list-type="picture-card"
-              :on-preview="handlePreview"
-            >
-              <el-icon class="avatar-uploader-icon"><Plus></Plus></el-icon>
+            <el-upload :action="baseUrl + '/v1/addimg/food'" class="avatar-uploader" :show-file-list="true"
+              :on-success="handleShopAvatarScucess" :before-upload="beforeAvatarUpload" :limit="1"
+              :on-exceed="handleExceed" :on-remove="handleRemove" list-type="picture-card" :on-preview="handlePreview">
+              <el-icon class="avatar-uploader-icon">
+                <Plus></Plus>
+              </el-icon>
             </el-upload>
             <el-dialog v-model="dialogVisible">
               <img w-full :src="dialogImageUrl" />
@@ -99,78 +63,38 @@
           </el-form-item>
 
           <el-form-item label="食品特点">
-            <el-select
-              v-model="foodForm.attributes"
-              multiple
-              placeholder="请选择"
-            >
-              <el-option
-                v-for="item in attributes"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
+            <el-select v-model="foodForm.attributes" multiple placeholder="请选择">
+              <el-option v-for="item in attributes" :key="item.value" :label="item.label" :value="item.value"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="食品规格">
-            <el-radio label="one" v-model="foodSpecs" class="radio"
-              >单规格</el-radio
-            >
-            <el-radio label="more" v-model="foodSpecs" class="radio"
-              >多规格</el-radio
-            >
+            <el-radio label="one" v-model="foodSpecs" class="radio">单规格</el-radio>
+            <el-radio label="more" v-model="foodSpecs" class="radio">多规格</el-radio>
           </el-form-item>
           <el-row v-if="foodSpecs === 'one'">
             <el-form-item label="包装费">
-              <el-input-number
-                v-model="foodForm.specs[0].packing_fee"
-                :min="0"
-                :max="100"
-              ></el-input-number>
+              <el-input-number v-model="foodForm.specs[0].packing_fee" :min="0" :max="100"></el-input-number>
             </el-form-item>
             <el-form-item label="价格">
-              <el-input-number
-                v-model="foodForm.specs[0].price"
-                :min="0"
-                :max="100"
-              ></el-input-number>
+              <el-input-number v-model="foodForm.specs[0].price" :min="0" :max="100"></el-input-number>
             </el-form-item>
           </el-row>
           <el-row v-else style="overflow: auto; text-align: center">
-            <el-button
-              type="primary"
-              @click="dialogFormVisible = true"
-              style="margin-bottom: 10px"
-              >添加规格</el-button
-            >
-            <el-table
-              :data="foodForm.specs"
-              style="margin-bottom: 20px"
-              :row-class-name="tableRowClassName"
-            >
+            <el-button type="primary" @click="dialogFormVisible = true" style="margin-bottom: 10px">添加规格</el-button>
+            <el-table :data="foodForm.specs" style="margin-bottom: 20px" :row-class-name="tableRowClassName">
               <el-table-column label="规格" prop="specs"></el-table-column>
-              <el-table-column
-                label="包装费"
-                prop="packing_fee"
-              ></el-table-column>
+              <el-table-column label="包装费" prop="packing_fee"></el-table-column>
               <el-table-column label="价格" prop="price"></el-table-column>
               <el-table-column label="操作">
                 <template #default="scope">
-                  <el-button
-                    type="danger"
-                    size="small"
-                    @click="handleDelte(scope.$index)"
-                    >删除</el-button
-                  >
+                  <el-button type="danger" size="small" @click="handleDelte(scope.$index)">删除</el-button>
                 </template>
               </el-table-column>
             </el-table>
           </el-row>
 
           <div class="form-submit">
-            <el-button type="primary" @click="addFood('foodForm')" round
-              >提交</el-button
-            >
+            <el-button type="primary" @click="addFood('foodForm')" round>提交</el-button>
           </div>
         </el-form>
 
@@ -181,28 +105,16 @@
               <el-input v-model="specsForm.specs"></el-input>
             </el-form-item>
             <el-form-item label="包装费" label-width="100px">
-              <el-input-number
-                v-model="specsForm.packing_fee"
-                :min="0"
-                :max="100"
-              ></el-input-number>
+              <el-input-number v-model="specsForm.packing_fee" :min="0" :max="100"></el-input-number>
             </el-form-item>
             <el-form-item label="价格" label-width="100px">
-              <el-input-number
-                v-model="specsForm.price"
-                :min="0"
-                :max="100"
-              ></el-input-number>
+              <el-input-number v-model="specsForm.price" :min="0" :max="100"></el-input-number>
             </el-form-item>
           </el-form>
           <div class="dialog-footer">
             <slot name="footer">
-              <el-button type="danger" @click="dialogFormVisible = false"
-                >取消</el-button
-              >
-              <el-button type="primary" @click="addspecs('specsForm')"
-                >确定</el-button
-              >
+              <el-button type="danger" @click="dialogFormVisible = false">取消</el-button>
+              <el-button type="primary" @click="addspecs('specsForm')">确定</el-button>
             </slot>
           </div>
         </el-dialog>
@@ -306,7 +218,7 @@ export default {
       this.restaurant_id = Math.ceil(Math.random() * 145);
       ElMessageBox.confirm(
         "添加食品需要选择一个商铺,先去选择商铺吗？",
-        "Warning",
+        "选择店铺",
         {
           confirmButtonText: "确定",
           cancelButtonText: "取消",
@@ -487,6 +399,7 @@ export default {
 
 <style lang="scss" scoped>
 @import "./../assets/style/mixin.scss";
+
 .form {
   min-width: 400px;
   margin-bottom: 30px;
@@ -508,24 +421,29 @@ export default {
   font-size: 20px;
   font-weight: 900;
 }
+
 .category_select {
   padding: 10px 30px 10px 10px;
 }
+
 .add_category_row {
   height: 0;
   overflow: hidden;
   transition: all 400ms;
   background: #f9fafc;
 }
+
 .showEdit {
   height: 185px;
 }
+
 .add_category {
   background: #f9fafc;
   padding: 10px 30px 0px 10px;
   // border: 1px solid #eaeefb;
   // border-top: none;
 }
+
 .add_category_button {
   text-align: center;
   line-height: 40px;
@@ -534,8 +452,10 @@ export default {
   border: 1px solid #eaeefb;
   border-top: none;
   transition: all 400ms;
+
   &:hover {
     background: #f9fafc;
+
     span,
     .edit_icon {
       color: #20a0ff;
