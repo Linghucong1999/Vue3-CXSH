dcf8de42a6d25a4d48d4195473571a9a5d31b53a
mika <2865911620@qq.com>
2024-01-02 00:40:28 +0800

    feat:中国地图数据

---
+++ src/page/visitor.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <v-chart></v-chart>
+    </div>
+</template>
+<script>
+import { getUserCity } from '../api/getData';
+import chinaMap from '@/assets/js/Map';
+export default {
+    data() {
+        return {
+            ROOT_PATH: 'https://geo.datav.aliyun.com/areas_v3/bound/100000_full.json',
+        }
+    },
+    created() {
+        this.getMapJson();
+    },
+    methods: {
+        async getMapJson() {
+            
+        }
+    }
+}
+
+</script>
+<style lang="scss" scoped></style>
